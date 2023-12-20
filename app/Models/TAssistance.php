d6b5c78fcf2688283ca26557ba183c218714e21e
Kevin Arnold Arias Figueroa <kaaf030191@gmail.com>
2023-12-20 11:21:27 -0500

    Init: Inicialización.
    Inicialización general.

---
+++ app/Models/TAssistance.php
@@ -0,0 +1,20 @@
+<?php 
+namespace App\Models;
+ 
+use Illuminate\Database\Eloquent\Model;
+ 
+class TAssistance extends Model
+{
+	protected $table='tassistance';
+	protected $primarykey='idAssistance';
+	protected $keyType='string';
+
+	public $incrementing=false;
+	public $timestamps=false;
+
+    public function TTeacher()
+	{
+		return $this->belongsTo('App\Models\TTeacher', 'idTeacher');
+	}
+}
+?>
---
+++ app/Models/TTeacher.php
@@ -0,0 +1,20 @@
+<?php 
+namespace App\Models;
+ 
+use Illuminate\Database\Eloquent\Model;
+ 
+class TTeacher extends Model
+{
+	protected $table='tteacher';
+	protected $primarykey='idTeacher';
+	protected $keyType='string';
+
+	public $incrementing=false;
+	public $timestamps=false;
+
+	public function TAssistance()
+	{
+		return $this->hasMany('App\Models\TAssistance', 'idTeacher');
+	}
+}
+?>
